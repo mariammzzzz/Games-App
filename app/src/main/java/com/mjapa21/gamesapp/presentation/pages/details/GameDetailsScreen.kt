e24b903b5025ec0865f39be1869cab6110ad4c2a
mjapa21 <mjapa21@freeuni.edu.ge>
2026-06-21 03:09:13 +0400

    Add error handling in details page

--- app/src/main/java/com/mjapa21/gamesapp/presentation/pages/details/GameDetailsScreen.kt
+++ app/src/main/java/com/mjapa21/gamesapp/presentation/pages/details/GameDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.mjapa21.gamesapp.presentation.pages.details
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import com.mjapa21.gamesapp.data.RetrofitInstance
 import com.mjapa21.gamesapp.data.model.GameDetails
 
@@ -18,17 +20,25 @@ import com.mjapa21.gamesapp.data.model.GameDetails
 @Composable
 fun GameDetailsScreen(gameId: Int) {
     var details by remember { mutableStateOf<GameDetails?>(null) }
+    val context = LocalContext.current
 
+    //TODO how would we pass the gameId to the viewmodel and have a proper implementation
     LaunchedEffect(Unit) {
-        val api = RetrofitInstance.createGamesApi()
-        details = api.getGameDetails(gameId)
+        try {
+            val api = RetrofitInstance.createGamesApi()
+            details = api.getGameDetails(gameId)
+        } catch (_: Exception) {
+            Toast.makeText(context, "Failed to load details", Toast.LENGTH_SHORT).show()
+        }
     }
 
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = androidx.compose.ui.Alignment.Center
-    ) {
+    details?.let {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = androidx.compose.ui.Alignment.Center
+        ) {
 
-        Text(modifier = Modifier.fillMaxWidth(), text = details?.description ?: "No details")
+            Text(modifier = Modifier.fillMaxWidth(), text = it.description)
+        }
     }
 }
